4b4325757d8f61ce11880e8ed3cdcf8c5b82a9cc
Tom Read Cutting <tr395@cam.ac.uk>
2017-11-15 00:17:20 +0000

    Complete Int specialisation pass

--- IntSpecialisationPass.hh
+++ IntSpecialisationPass.hh
@@ -12,7 +12,7 @@
 #include "llvm/IR/InstVisitor.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
-
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
 
 
 using namespace llvm;
@@ -30,6 +30,29 @@ const std::string MUL_FUNCTION_NAME = "mysoreScriptMul";
 const std::string SUB_FUNCTION_NAME = "mysoreScriptSub";
 const std::string DIV_FUNCTION_NAME = "mysoreScriptDiv";
 
+Value *getAsSmallInt2(IRBuilder<>& b, Type* ObjIntTy, Value *i)
+{
+	if (i->getType()->isPointerTy())
+	{
+		return b.CreatePtrToInt(i, ObjIntTy);
+	}
+	return b.CreateBitCast(i, ObjIntTy);
+}
+
+Value *compileSmallInt2(IRBuilder<>& b, Type* ObjIntTy, Value *i)
+{
+  i = b.CreateShl(i, ConstantInt::get(ObjIntTy, 3));
+  return b.CreateOr(i, ConstantInt::get(ObjIntTy, 1));
+}
+
+Value *getAsObject2(IRBuilder<>& b, Type* ObjPtrTy, Value *i)
+{
+  if (i->getType()->isPointerTy())
+  {
+    return b.CreateBitCast(i, ObjPtrTy);
+  }
+  return b.CreateIntToPtr(i, ObjPtrTy);
+}
 
 struct IntSpecialisationPass : FunctionPass, InstVisitor<IntSpecialisationPass>
 {
@@ -69,59 +92,202 @@ struct IntSpecialisationPass : FunctionPass, InstVisitor<IntSpecialisationPass>
     return false;
   }
 
+  bool isAddition(const Function* func) {
+    return ADD_FUNCTION_NAME == func->getName();
+  }
+
+  bool isMultiplication(const Function* func) {
+    return MUL_FUNCTION_NAME == func->getName();
+  }
+
+  bool isSubtraction(const Function* func) {
+    return SUB_FUNCTION_NAME == func->getName();
+  }
+
+  bool isDivision(const Function* func) {
+    return DIV_FUNCTION_NAME == func->getName();
+  }
+
+  bool isBinaryOp(const Function* func) {
+    return isAddition(func) ||
+           isMultiplication(func) ||
+           isSubtraction(func) ||
+           isDivision(func);
+  }
+
   bool runOnFunction(Function &F) override {
 
     visit(F);
-
+    LLVMContext& C = F.getContext();
     llvm::outs() << "Starting Type Specialisation Pass\n";
 
-    llvm::SmallVector<int, 16> functionCount;
 
-    std::set<const Value*> intsToCheck;
-    // std::set<Value*> returnVals
+    // In this vector of vectors, we will stall contiguous sequences of calls
+    // to addition operations.
+    // that we can "specialise"
+    std::vector<std::vector<CallInst*>> callSequences;
 
-    // Alternatively, we can loop over each basic block and then over each
-    // instruction and inspect them individually:
-    for (const llvm::BasicBlock& BB : F) {
-      int numFunctions = 0;
-      for(auto rit = BB.rbegin(); rit != BB.rend(); ++rit) {
-        const llvm::Instruction& I = *rit;
-      //for (const llvm::Instruction& I : BB) {
-        if(const CallInst* arithmeticInst = dyn_cast<CallInst>(&I)) {
-          const Function* arithmeticFunc = arithmeticInst->getCalledFunction();
-          if(ADD_FUNCTION_NAME == arithmeticFunc->getName()) {
-            const Value* leftArg = arithmeticInst->getArgOperand(0);
-            const Value* rightArg = arithmeticInst->getArgOperand(1);
-            // const Value* ret = arithmeticInst->getArgOperand(2);
-            llvm::outs() << "args{\n";
-            llvm::outs() << "  l: " << leftArg << "\n";
-            llvm::outs() << "  r: " << rightArg<< "\n";
-            llvm::outs() << "  v: " << arithmeticInst << "\n";
-            llvm::outs() << "}\n";
-            if(intsToCheck.count(arithmeticInst) > 0) {
-              llvm::outs() << "HERE!!\n";
-              intsToCheck.erase(arithmeticInst);
-            }
-            intsToCheck.insert(leftArg);
-            intsToCheck.insert(rightArg);
-            numFunctions++;
+    // Iterate through every basic block and find contiguous sequences of binary operations
+    for (BasicBlock& basicBlock: F) {
+      std::vector<CallInst*> callSequence;
+      bool inContiguousSequence = false;
+      for(Instruction& instruction: basicBlock) {
+        bool isBinaryOps = false;
+        if(CallInst* functionCall = dyn_cast<CallInst>(&instruction)) {
+          Function* arithmeticFunc = functionCall->getCalledFunction();
+          if(isBinaryOp(arithmeticFunc)) {
+            callSequence.push_back(functionCall);
+            isBinaryOps = true;
+          }
+        }
+
+        if(!isBinaryOps && inContiguousSequence) {
+          callSequences.push_back(callSequence);
+          callSequence.clear();
+        }
+      }
+      callSequences.push_back(callSequence);
+    }
+
+    Type* ObjIntTy = Type::getInt64Ty(F.getContext());
+    Type* ObjPtrTy = Type::getInt8PtrTy(F.getContext());
+
+    for(std::vector<CallInst*>& callSequence: callSequences) {
+      llvm::outs() << "Analysing a sequence of contiguous calls \n";
+      CallInst* firstBinaryOpCall = callSequence.front();
+      CallInst* lastBinaryOpCall = callSequence.back();
+      IRBuilder<> irBuilder(firstBinaryOpCall);
+
+      // Symbolically keep track of which results map to which symbolic values.
+      std::map<Value*, int> resultVals;
+      int symbolicInt = 0;
+      for(CallInst* arithmeticInst: callSequence) {
+          if(resultVals.count(arithmeticInst) < 1) {
+            resultVals.insert(std::pair<Value*, int>(arithmeticInst, symbolicInt++));
+          }
+      }
+
+      ///
+      /// Find all the integers that need to be checked, and create the LLVM IR
+      /// that performs the checks
+      ///
+      Value* onlyIntegerAdditions = nullptr;
+      {
+        std::set<Value*> intsToCheck;
+        for(auto rit = callSequence.rbegin(); rit != callSequence.rend(); ++rit) {
+          CallInst* arithmeticInst = *rit;
+          Value* leftArg = arithmeticInst->getArgOperand(0);
+          Value* rightArg = arithmeticInst->getArgOperand(1);
+
+          if(intsToCheck.count(arithmeticInst) > 0) {
+            intsToCheck.erase(arithmeticInst);
+          }
+          intsToCheck.insert(leftArg);
+          intsToCheck.insert(rightArg);
+        }
+
+        Value* prevInt = nullptr;
+        for(Value* intToCheck: intsToCheck) {
+          intToCheck = getAsSmallInt2(irBuilder, ObjIntTy, intToCheck);
+          if(prevInt == nullptr) {
+            prevInt = intToCheck;
+            continue;
+          }
+          // And them together.  If they are both small ints, then the low bits of the
+          // result will be 001.
+          Value *isSmallInt = irBuilder.CreateAnd(intToCheck, prevInt);
+          // Now mask off the low 3 bits.
+          isSmallInt = irBuilder.CreateAnd(isSmallInt, ConstantInt::get(ObjIntTy, 7));
+          // If the low three bits are 001, then it is a small integer
+          isSmallInt = irBuilder.CreateICmpEQ(isSmallInt, ConstantInt::get(ObjIntTy, 1));
+
+          if(onlyIntegerAdditions == nullptr) {
+            onlyIntegerAdditions = isSmallInt;
+          } else {
+            onlyIntegerAdditions = irBuilder.CreateAnd(isSmallInt, onlyIntegerAdditions);
           }
         }
       }
+      ///
+      /// End Integer Checking
+      ///
 
-      functionCount.push_back(numFunctions);
-    }
 
-    {
-      llvm::outs() << "LLVM_PASS:";
-      for(const int numFunctions: functionCount) {
-        llvm::outs() << numFunctions << ",\n IntsToCheck: \n";
-        for(auto val: intsToCheck)
-          llvm::outs() << val << "\n";
+      TerminatorInst* rawIntegerBlockTerm = nullptr;
+      TerminatorInst* safeArithmeticBlockTerm = nullptr;
+
+      // Split the block
+      SplitBlockAndInsertIfThenElse(onlyIntegerAdditions, firstBinaryOpCall,
+                                    &rawIntegerBlockTerm,
+                                    &safeArithmeticBlockTerm,
+                                    nullptr);
+
+      std::vector<Value*> intResults;
+      for(CallInst* binaryOpCall: callSequence) {
+        PHINode* phiNode = PHINode::Create(binaryOpCall->getFunctionType()->getReturnType(), 2, "binaryOpResultPhi");
+        phiNode->insertAfter(lastBinaryOpCall);
+
+        // We want to replace all future instances of the use of the result of this binary operation call
+        // that aren't in the current sequence of binary operation calls with the phi node
+        // which collects the raw integer result and the safe one.
+        for(Use& use: binaryOpCall->materialized_uses()) {
+          User* user = use.getUser();
+          if(CallInst* call = dyn_cast<CallInst>(user)) {
+            if(std::find(callSequence.begin(), callSequence.end(), call) != callSequence.end()) {
+              continue;
+            }
+          }
+          user->setOperand(use.getOperandNo(), phiNode);
+        }
+        binaryOpCall->moveBefore(safeArithmeticBlockTerm);
+
+        irBuilder.SetInsertPoint(rawIntegerBlockTerm);
+
+        // The searches here need to be done so that we ge the calculated results
+        // from our arithmetic if we need to, not those the other basic block.
+        Value* lhsInt = binaryOpCall->getArgOperand(0);
+        {
+          auto search = resultVals.find(lhsInt);
+          if(search != resultVals.end()) {
+            int resultIndex = search->second;
+            lhsInt = intResults[resultIndex];
+          } else {
+            lhsInt = irBuilder.CreateAShr(getAsSmallInt2(irBuilder, ObjIntTy, lhsInt), ConstantInt::get(ObjIntTy, 3));
+          }
+        }
+        Value* rhsInt = binaryOpCall->getArgOperand(1);
+        {
+          auto search = resultVals.find(rhsInt);
+          if(search != resultVals.end()) {
+            int resultIndex = search->second;
+            rhsInt = intResults[resultIndex];
+          } else {
+            rhsInt = irBuilder.CreateAShr(getAsSmallInt2(irBuilder, ObjIntTy, rhsInt), ConstantInt::get(ObjIntTy, 3));
+          }
+        }
+
+
+        const Function* binaryOpFunc = binaryOpCall->getCalledFunction();
+        Value* intResult;
+        if(isAddition(binaryOpFunc)) {
+          intResult = irBuilder.CreateAdd(lhsInt, rhsInt);
+        } else if(isSubtraction(binaryOpFunc)) {
+          intResult = irBuilder.CreateSub(lhsInt, rhsInt);
+        } else if(isMultiplication(binaryOpFunc)) {
+          intResult = irBuilder.CreateMul(lhsInt, rhsInt);
+        } else /*if(isDivision(binaryOpFunc))*/ {
+          intResult = irBuilder.CreateSDiv(lhsInt, rhsInt);
+        }
+
+        Value* objIntResult = getAsObject2(irBuilder, ObjPtrTy, compileSmallInt2(irBuilder, ObjIntTy, intResult));
+
+        intResults.push_back(intResult);
+
+        phiNode->addIncoming(objIntResult, rawIntegerBlockTerm->getParent());
+        phiNode->addIncoming(binaryOpCall, safeArithmeticBlockTerm->getParent());
       }
-      llvm::outs() << "\n";
     }
-
+    llvm::outs() << "Ending PASS\n";
     return true;
   }
 }; // END STRUCT
